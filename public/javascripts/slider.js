89b059a6af44414a28642cae4a434cc6ba31b1e4
kai5555 <deneubourgkai@gmail.com>
2022-12-06 11:01:01 +0100

    style fixes

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -3,7 +3,7 @@ var output = document.getElementById("value")
 var slider = document.getElementById("range").oninput = function () {
 
     var value = (this.value - this.min) / (this.max - this.min) * 100
-    output.innerHTML = this.value;
+    output.innerHTML = this.value + '&nbsp';
 
 }
 
