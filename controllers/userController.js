5fa4fc30d763aae6922a92628421b2263bc7a69b
WoutVanS <64586732+WoutVanS@users.noreply.github.com>
2022-12-06 13:19:16 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -111,9 +111,7 @@ exports.user_register_post = [
         .trim()
         .isLength({ min: 1 })
         .escape()
-        .withMessage("Family name must be specified.")
-        .isAlphanumeric()
-        .withMessage("Family name has non-alphanumeric characters."),
+        .withMessage("Family name must be specified."),
     body("date_of_birth", "Invalid date of birth")
         .optional({ checkFalsy: true })
         .isISO8601()
